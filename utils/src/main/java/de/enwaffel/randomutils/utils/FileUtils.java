2b5d5553c1240458f1cb841fb2ae53095170eaec
EnWaffel <89415364+EnWaffel@users.noreply.github.com>
2022-05-16 17:03:37 +0200

    new Project

--- utils/src/main/java/de/enwaffel/randomutils/utils/FileUtils.java
+++ utils/src/main/java/de/enwaffel/randomutils/utils/FileUtils.java
@@ -1,7 +1,41 @@
 package de.enwaffel.randomutils.utils;
 
+import java.io.FileInputStream;
+import java.nio.file.Files;
+
 public class FileUtils {
 
+    // read
 
+    public String readFile(FileOrPath fileOrPath, int maxBytes) {
+        try {
+            FileInputStream fis = new FileInputStream(fileOrPath.getFile());
+            byte[] buffer = new byte[maxBytes];
+            while (fis.read(buffer, 0, maxBytes) >= 0);
+            return new String(buffer);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    public String readFile(FileOrPath fileOrPath) {
+        try {
+            return new String(Files.readAllBytes(fileOrPath.getFile().toPath()));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    // write
+
+    public void writeFile(FileOrPath fileOrPath) {
+        try {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
--- utils/src/main/java/de/enwaffel/randomutils/utils/Utils.java
+++ utils/src/main/java/de/enwaffel/randomutils/utils/Utils.java
@@ -2,6 +2,7 @@ package de.enwaffel.randomutils.utils;
 
 public class Utils {
 
+    public static final ValidateUtils validate = new ValidateUtils();
     public static final FileUtils file = new FileUtils();
 
 }
